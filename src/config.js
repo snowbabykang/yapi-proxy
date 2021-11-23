aa0ec171d5dda2dd217102497f5d813f39a48a7a
kangx <kangxue@julive.com>
2021-11-23 15:24:16 +0800

    [update] 提交配置

--- src/config.js
+++ src/config.js
@@ -4,7 +4,8 @@ module.exports = {
     apiUrl: 'https://yapi.yunjiai.cn',
     // 项目id和token数组
     tokens: [
-        { name: 'AI管家音响管理后台', id: '38', token: 'e38d8b2087b12241cddefbba2a9c26f0cd77fa6a054dd70bb0b52150ec0905a8' }
+        { name: 'AI管家音响管理后台', id: '38', token: 'e38d8b2087b12241cddefbba2a9c26f0cd77fa6a054dd70bb0b52150ec0905a8' },
+        { name: 'HDOS商家版web', id: '309', token: '4b0720a69d1a7370d7484bd1d4910bb6a51d06fa8d6f08689dbeb7779e6b3945' }
     ],
     "commonTokens": [
         {
